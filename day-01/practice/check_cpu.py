cee03cb4e60d5a06988f891bbdaf50d7354f8b23
Imtiaz <Imtiaz.4.us@gmail.com>
2026-02-04 15:28:50 -0500

    Update check_cpu.py

--- day-01/practice/check_cpu.py
+++ day-01/practice/check_cpu.py
@@ -1,18 +1,62 @@
-import psutil # import the lib from pypi
+import psutil
 
-# aapko kaam karna hai ki user se CPU threshold lo
-# current cpu usage pata karo 
-# agar cpu usage threshold se zyada hua, email kar do
+def get_cpu_threshold():
 
-def check_cpu_threshold():
-    cpu_threshold = int(input("Enter the CPU Threshold")) # DONE
+    user_cpu = int(input("Enter cpu threshold"))
 
-    current_cpu = psutil.cpu_percent(interval=1) # DONE
-    print("Current CPU %: ",current_cpu)
-    if current_cpu > cpu_threshold:  # DONE
-        print("CPU Alert Email sent...") # dummy email
+    current_cpu = psutil.cpu_percent(interval=1)
+    print("current cpu%: ", current_cpu)
+
+    if current_cpu > user_cpu:
+
+        print("cpu alert email sent...")
     else:
-        print("CPU in Safe state...")
+        print("cpu usage is safe")
 
 
-check_cpu_threshold()
+def get_disk():
+
+    user_disk = float(input("enter your system disk (%): "))
+
+
+    disk = psutil.disk_usage("/")
+    current_disk_percent = disk.percent
+
+    print("present disk usage:",current_disk_percent, "%")
+
+
+    if current_disk_percent< user_disk:
+        print("system healthy")
+    else:
+        print("not healthy")
+
+# cpu and disk monitoring script
+# cpu and disk mon
+
+###############################################
+# def get_disk():
+#     user_disk = float(input("enter your system disk (%): "))
+
+#     disk = psutil.disk_usage("/")     # returns sdiskusage object
+#     current_disk_percent = disk.percent
+
+#     print("present disk usage:", current_disk_percent, "%")
+
+#     if current_disk_percent < user_disk:
+#         print("system healthy")
+#     else:
+#         print("not healthy")
+
+
+
+
+
+
+get_cpu_threshold()
+get_disk()           
+
+
+
+
+
+
