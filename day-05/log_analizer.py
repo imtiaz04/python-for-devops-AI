f1d0e18324432af3722caae03d6d9ae910808168
Imtiaz <Imtiaz.4.us@gmail.com>
2026-02-09 14:29:02 -0500

    added log analizer, output file to day 5 folder

---
+++ day-05/log_analizer.py
@@ -0,0 +1,41 @@
+import pdb
+import json
+
+def read_logs():
+    with open("app.log", "r") as file: #file open
+        #lines.extend(file.readlines()) #operation to read the file
+        return file.readlines()
+
+
+def analyze(lines):
+    
+    log_count = {
+        "INFO": 0,
+        "WARNING": 0,
+        "ERROR": 0
+        
+    }
+    for line in lines:
+
+        if "INFO" in line:
+            log_count.update({"INFO":log_count["INFO"]+1})
+        elif "WARNING" in line:
+            log_count.update({"WARNING" :log_count["WARNING"]+1})
+        elif "ERROR" in line:
+            log_count.update({"ERROR": log_count["ERROR"]+1})
+        else:
+            pass
+    return log_count
+
+def write_json(counts):
+        with open("output.json", "w+") as json_file:
+            json.dump(counts, json_file, indent=4)
+
+
+lines =read_logs()
+
+counts = analyze(lines) 
+
+write_json(counts)
+
+print("log counts are: ", counts)  
